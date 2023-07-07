16b871a94fb864e7ce4228c7259405290e182c9b
wyekings <wyekings@outlook.com>
2023-07-07 23:36:28 +0800

    Add log

--- lib/ui/counter/counter.dart
+++ lib/ui/counter/counter.dart
@@ -1,9 +1,12 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class CounterPage extends StatefulWidget {
-  const CounterPage({super.key, required this.title});
+  const CounterPage({super.key, required this.title, this.initValue = 0});
 
   final String title;
+  final int initValue;
 
   @override
   State<CounterPage> createState() => _CounterPageState();
@@ -11,6 +14,44 @@ class CounterPage extends StatefulWidget {
 
 class _CounterPageState extends State<CounterPage> {
   int _counter = 0;
+
+  @override
+  void initState() {
+    _counter = widget.initValue;
+    super.initState();
+    debugPrint('initState');
+    // log('initState');
+  }
+
+  @override
+  void didChangeDependencies() {
+    debugPrint('didChangeDependencies');
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant CounterPage oldWidget) {
+    debugPrint('didUpdateWidget');
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void reassemble() {
+    debugPrint('reassemble');
+    super.reassemble();
+  }
+
+  @override
+  void deactivate() {
+    debugPrint('deactivate');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    debugPrint('dispose');
+    super.dispose();
+  }
 
   void _incrementCounter() {
     setState(() {
